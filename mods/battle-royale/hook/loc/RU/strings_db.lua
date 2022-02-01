f7a4d05826333ba0b2080f3bd74323f1390844e0
Some Developer <somedeveloper05@gmail.com>
2022-02-01 23:35:59 +0300

    Added russian localization

---
+++ mods/battle-royale/hook/loc/RU/strings_db.lua
@@ -0,0 +1,100 @@
+-- delay keys
+delay_label="Королевская битва: время на подготовку"
+delay_help="Определяет время до начала сужения карты"
+
+delay_values_none_text="Откл."
+delay_values_very_short_text="Очень маленькое"
+delay_values_short_text="Маленькое"
+delay_values_medium_text="Среднее"
+delay_values_long_text="Долгое"
+
+delay_values_none_help="Отключает задержку. Сужение карты начинается со старта игры."
+delay_values_very_short_help="Сужение карты начинается через 60 секунд после начала игры."
+delay_values_short_help="Сужение карты начинается через 120 секунд после начала игры."
+delay_values_medium_help="Сужение карты начинается через 240 секунд после начала игры."
+delay_values_long_help="Сужение карты начинается через 360 секунд после начала игры."
+
+
+--type keys
+type_label="Тип сужения карты"
+type_help="Определяет каким образом карта будет уменьшаться."
+
+type_values_random_text="Случайное"
+type_values_square_text="Со всех сторон"
+type_values_clockwise_text="По часовой стрелке"
+type_values_counterclockwise_text="Против часовой стрелки"
+
+type_values_random_help="Сторона сужения определяется случайно."
+type_values_square_help="Сужение происходит одновременно со всех сторон, но площадь сужения каждой из сторон снижена."
+type_values_clockwise_help="Сужение происходит по часовой стрелке, начальная сторона определяется случайно."
+type_values_counterclockwise_help="Сужение происходит против часовой стрелки, начальная сторона определяется случайно."
+
+--interval keys
+interval_label="Интервал сужения"
+interval_help="Определяет на сколько быстро сужается карта."
+
+interval_values_slow_text="Медленно"
+interval_values_medium_text="Средне"
+interval_values_fast_text="Быстро"
+interval_values_hyper_text="Очень быстро"
+
+interval_values_slow_help="Карта уменьшается каждые 180 секунд."
+interval_values_medium_help="Карта уменьшается каждые 140 секунд."
+interval_values_fast_help="Карта уменьшается каждые 100 секунд."
+interval_values_hyper_help="Карта уменьшается каждые 60 секунд."
+
+--packages interval keys
+packages_interval_label="Интервал появления юнитов"
+packages_interval_help="Определяет с какой периодичностью появляются паки нейтральных юнитов."
+
+packages_interval_values_off_text="Откл."
+packages_interval_values_slow_text="Медленно"
+packages_interval_values_medium_text="Средне"
+packages_interval_values_fast_text="Быстро"
+packages_interval_values_hyper_text="Оч. быстро"
+
+packages_interval_values_off_help="Отключает появление нейтральных юнитов на карте."
+packages_interval_values_slow_help="Паки юнитов появляются каждые 60 секунд."
+packages_interval_values_medium_help="Паки юнитов появляются каждые 40 секунд."
+packages_interval_values_fast_help="Паки юнитов появляются каждые 20 секунд."
+packages_interval_values_hyper_help="Паки юнитов появляются каждые 10 секунд."
+
+
+--packages curve keys
+packages_curve_label="Технологический уровень юнитов"
+packages_curve_help="Определяет на сколько быстро повышается технологический уровень юнитов."
+
+packages_curve_values_slow_text="Медленно"
+packages_curve_values_medium_text="Средне"
+packages_curve_values_fast_text="Быстро"
+packages_curve_values_hyper_text="Оч. быстро"
+
+packages_curve_values_slow_help="Т3 юниты появятся на 30 минуте игры."
+packages_curve_values_medium_help="Т3 юниты появятся на 25 минуте игры."
+packages_curve_values_fast_help="Т3 юниты появятся на 20 минуте игры."
+packages_curve_values_hyper_help="Т3 юниты появятся на 15 минуте игры."
+
+--packages amount keys
+packages_amount_label="Количество юнитов в паках"
+packages_amount_help="Определяет сколько юнитов появится в одном паке."
+
+packages_amount_values_normal_text="Стандартное"
+packages_amount_values_double_text="Двойное"
+
+packages_amount_values_normal_help="Стандартное число юнитов в паке."
+packages_amount_values_double_help="Двойное число юнитов в паке."
+
+--ui keys
+ui_shrinking_start="Время до начала сужения карты: "
+ui_shrinking_next="Время до следующего сужения: "
+ui_shrinking="Сужение"
+
+ui_battle_royale="Королевская Битва"
+ui_help="Помощь"
+ui_care_packages="Паки юнитов"
+ui_care_packages_time="Время до следующего появления юнитов: "
+
+ui_help_text_1="Паки юнитов появляются по всей карте. Они содержат юниты, которые вы можете захватить, зареклеймить или уничтожить. Маяк — это быстрый доступ к паку юнитов. Что бы ни случилось с маяком, происходит и с остальными юнитами."
+ui_help_text_2="Например, если вы захватите маяк, юниты, которые идут с маяком, станут вашими. Радар и разведчики полезны для поиска маяков. Место появления последнего пака юнитов отображается бирюзовым квадратом на предварительном просмотре карты."
+ui_help_text_3="Со временем карта будет уменьшаться. Красная линия показывает, где будет новая граница карты. Все юниты, которые находятся снаружи границы, уничтожаются во время сокращения."
+
