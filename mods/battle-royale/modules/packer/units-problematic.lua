54f1303e5c1bdbdc40ed480545a92c285f31a651
Some Developer <somedeveloper05@gmail.com>
2022-02-15 14:32:28 +0300

    Fixed beacon appearing inside some units

---
+++ mods/battle-royale/modules/packer/units-problematic.lua
@@ -0,0 +1,14 @@
+
+--- List of units for which you need to adjust the spawn radius, because  without adjustment, the beacon appears inside the unit.
+--- id - unit bp
+--- radiusIncrease - the value by which to increase the radius
+UnitTable = {
+    tempest = {
+        id = 'UAS0401',
+        radiusIncrease = 6
+    },
+    fatboy = {
+        id = 'UEL0401',
+        radiusIncrease = 2
+    },
+}
