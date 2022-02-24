a721551b560d7d3e1cb2244f60aa983f080415f4
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-02-24 16:28:54 +0100

    Improve fidelity of Seraphim beacons

--- mods/battle-royale/units/xsb5205/XSB5205_Script.lua
+++ mods/battle-royale/units/xsb5205/XSB5205_Script.lua
@@ -1,25 +1,58 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/URB5103/URB5103_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Cybran Quantum Gate Beacon Unit
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
 
-local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
 
-URB5103 = Class(CStructureUnit) {
-	FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-	FxTransportBeaconScale =1,
-	
+local Entity = import('/lua/sim/entity.lua').Entity
+local CStructureUnit = import('/lua/seraphimunits.lua').SStructureUnit
+
+XSB5205 = Class(CStructureUnit) {
 	OnCreate = function(self)
 		CStructureUnit.OnCreate(self)
-		for k, v in self.FxTransportBeacon do
-			self.Trash:Add(CreateAttachedEmitter(self, 0,self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
-		end
+
+        -- special effects
+		local hoverfx = CreateAttachedEmitter(self, -1, self:GetArmy(), "/mods/battle-royale/effects/seraphim_beacon_hover.bp")
+		hoverfx:OffsetEmitter(0, 0.7, 0)
+		self.Trash:Add(hoverfx)
+
+		local distortfx = CreateAttachedEmitter(self, -1, self:GetArmy(), "/mods/battle-royale/effects/seraphim_beacon_distortion_02.bp")
+		distortfx:OffsetEmitter(0, 0.7, 0)
+		self.Trash:Add(distortfx)
+
+        -- rotate a bit
+        local r = CreateRotator(self, 'ball1', 'y')
+        self.Trash:Add(self.r)
+        r:SetSpinDown(false)
+        r:SetTargetSpeed(90)
+        r:SetAccel(20)
+
+        local r = CreateRotator(self, 'ball2', 'y')
+        self.Trash:Add(self.r)
+        r:SetSpinDown(false)
+        r:SetTargetSpeed(-60)
+        r:SetAccel(20)
+
+        -- add shield bulbs
+        self:AddShieldToBone('ball1')
+        self:AddShieldToBone('ball2')
 	end,
+
+    --- Adds a small shield to a bone of this unit
+    AddShieldToBone = function(self, bone)
+
+        local shield = Entity()
+        shield:AttachTo(self, bone)
+        shield:SetMesh('/effects/entities/SeraphimShield01/SeraphimShield01_mesh')
+        shield:SetDrawScale(1)
+        shield:SetCollisionShape('Sphere', 0, 0, 0, 0.75)
+
+        shield:SetVizToAllies('Intel')
+        shield:SetVizToNeutrals('Intel')
+        shield:SetVizToEnemies('Intel')
+        
+        shield.OnCollisionCheck = function(self, other, firingWeapon)
+            CreateEmitterOnEntity(other, other.Army, '/mods/battle-royale/effects/seraphim_beacon_impact.bp')
+        end
+        
+        self.Trash:Add(shield)
+    end,
 }
 
-TypeClass = URB5103
+TypeClass = XSB5205
--- mods/battle-royale/units/xsb5305/XSB5305_Script.lua
+++ mods/battle-royale/units/xsb5305/XSB5305_Script.lua
@@ -1,13 +1,13 @@
 
+
+local Entity = import('/lua/sim/entity.lua').Entity
 local CStructureUnit = import('/lua/seraphimunits.lua').SStructureUnit
 
 XSB5305 = Class(CStructureUnit) {
-	FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-	FxTransportBeaconScale =1,
-
 	OnCreate = function(self)
 		CStructureUnit.OnCreate(self)
 
+        -- special effects
 		local hoverfx = CreateAttachedEmitter(self, -1, self:GetArmy(), "/mods/battle-royale/effects/seraphim_beacon_hover.bp")
 		hoverfx:OffsetEmitter(0, 0.7, 0)
 		self.Trash:Add(hoverfx)
@@ -35,7 +35,31 @@ XSB5305 = Class(CStructureUnit) {
         r:SetTargetSpeed(30)
         r:SetAccel(20)
 
+        -- add shield bulbs
+        self:AddShieldToBone('ball1')
+        self:AddShieldToBone('ball2')
+        self:AddShieldToBone('ball3')
 	end,
+
+    --- Adds a small shield to a bone of this unit
+    AddShieldToBone = function(self, bone)
+
+        local shield = Entity()
+        shield:AttachTo(self, bone)
+        shield:SetMesh('/effects/entities/SeraphimShield01/SeraphimShield01_mesh')
+        shield:SetDrawScale(1)
+        shield:SetCollisionShape('Sphere', 0, 0, 0, 0.75)
+
+        shield:SetVizToAllies('Intel')
+        shield:SetVizToNeutrals('Intel')
+        shield:SetVizToEnemies('Intel')
+        
+        shield.OnCollisionCheck = function(self, other, firingWeapon)
+            CreateEmitterOnEntity(other, other.Army, '/mods/battle-royale/effects/seraphim_beacon_impact.bp')
+        end
+        
+        self.Trash:Add(shield)
+    end,
 }
 
 TypeClass = XSB5305
