597a5a05e8b6ae3b1b4dc42a91220393ab1dcb40
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-02-25 11:19:20 +0100

    Working on Aeon and Seraphim beacons

--- mods/battle-royale/units/uab5105/UAB5105_Script.lua
+++ mods/battle-royale/units/uab5105/UAB5105_Script.lua
@@ -1,29 +1,28 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB5103/UAB5103_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Aeon Quantum Gate Beacon Unit
-#**
-#**  Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
 
 local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
 
+local QuanticClusterHit = import('/lua/EffectTemplates.lua').Aeon_QuanticClusterHit
+local QuanticClusterMuzzleFlash = import('/lua/EffectTemplates.lua').Aeon_QuanticClusterMuzzleFlash
+
 UAB5105 = Class(AStructureUnit) {
-	FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-	FxTransportBeaconScale =1,
 	
+    FxDamage1 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+    FxDamage2 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+    FxDamage3 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+
 	OnCreate = function(self)
 		AStructureUnit.OnCreate(self)
         
-        -- create beacon effect
-        for k = 0, self:GetBoneCount() do 
-            local name = self:GetBoneName(k)
-            if string.find(name, "light") then 
-                self.Trash:Add(CreateAttachedEmitter(self, k, self:GetArmy(), "/mods/battle-royale/effects/beacon_light.bp"))
-            end
-        end
+        local effect = CreateEmitterAtBone(self, 'light', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_01.bp")
+        effect:OffsetEmitter(0, 0.05, 0)
+        effect:ScaleEmitter(0.25)
+        self.Trash:Add(effect)
+
+        local effect = CreateEmitterAtBone(self, 'light', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_02.bp")
+        effect:OffsetEmitter(0, -0.1, 0)
+        effect:ScaleEmitter(0.6)
+        self.Trash:Add(effect)
+
 
         -- rotate a bit
         local r = CreateRotator(self, 'UAB5105', 'y')
@@ -32,6 +31,28 @@ UAB5105 = Class(AStructureUnit) {
         r:SetTargetSpeed(30)
         r:SetAccel(20)
 	end,
+
+    DeathThread = function(self, instigator, type, overkillRatio)
+
+        for k, effect in QuanticClusterHit do 
+            local effect = CreateEmitterAtBone(self, 'light', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        for k, effect in QuanticClusterMuzzleFlash do 
+            local effect = CreateEmitterAtBone(self, 'light', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        WaitSeconds(0.1)
+
+        self:HideBone('UAB5105', false)
+
+        AStructureUnit.DeathThread(self, instigator, type, overkillRatio)
+    end,
+
+    -- override to disable default damage effects
+    CreateDestructionEffects = function() end,
 }
 
 TypeClass = UAB5105
--- mods/battle-royale/units/uab5205/UAB5205_Script.lua
+++ mods/battle-royale/units/uab5205/UAB5205_Script.lua
@@ -1,25 +1,92 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB5103/UAB5103_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Aeon Quantum Gate Beacon Unit
-#**
-#**  Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
 
 local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
 
-UAB5103 = Class(AStructureUnit) {
-	FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-	FxTransportBeaconScale =1,
-	
+local QuanticClusterHit = import('/lua/EffectTemplates.lua').Aeon_QuanticClusterHit
+local QuanticClusterMuzzleFlash = import('/lua/EffectTemplates.lua').Aeon_QuanticClusterMuzzleFlash
+
+UAB5205 = Class(AStructureUnit) {
+    FxDamage1 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+    FxDamage2 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+    FxDamage3 = { import('/lua/EffectTemplates.lua').DamageSparks01 },
+
 	OnCreate = function(self)
 		AStructureUnit.OnCreate(self)
-		for k, v in self.FxTransportBeacon do
-			self.Trash:Add(CreateAttachedEmitter(self, 0,self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
-		end
+        
+        local effect = CreateEmitterAtBone(self, 'light', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_01.bp")
+        effect:OffsetEmitter(0, 0.05, 0)
+        effect:ScaleEmitter(0.25)
+        self.Trash:Add(effect)
+
+        local effect = CreateEmitterAtBone(self, 'light', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_02.bp")
+        effect:OffsetEmitter(0, -0.1, 0)
+        effect:ScaleEmitter(0.6)
+        self.Trash:Add(effect)
+
+        local effect = CreateEmitterAtBone(self, 'light_2', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_01.bp")
+        effect:OffsetEmitter(0, 0.05, 0)
+        effect:ScaleEmitter(0.25)
+        self.Trash:Add(effect)
+
+        local effect = CreateEmitterAtBone(self, 'light_2', self.Army, "/mods/battle-royale/effects/aeon_beacon_idle_effect_02.bp")
+        effect:OffsetEmitter(0, -0.1, 0)
+        effect:ScaleEmitter(0.6)
+        self.Trash:Add(effect)
+
+
+        -- rotate a bit
+        local r = CreateRotator(self, 'UAB5205', 'y')
+        self.Trash:Add(self.r)
+        r:SetSpinDown(false)
+        r:SetTargetSpeed(30)
+        r:SetAccel(20)
+
+        -- rotate a bit
+        local r = CreateRotator(self, 'UAB5205_2', 'y')
+        self.Trash:Add(self.r)
+        r:SetSpinDown(false)
+        r:SetTargetSpeed(30)
+        r:SetAccel(20)
 	end,
+
+    DeathThread = function(self, instigator, type, overkillRatio)
+
+        for k, effect in QuanticClusterHit do 
+            local effect = CreateEmitterAtBone(self, 'light', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        for k, effect in QuanticClusterMuzzleFlash do 
+            local effect = CreateEmitterAtBone(self, 'light', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        WaitSeconds(0.1)
+
+        self:HideBone('UAB5205', false)
+
+        WaitSeconds(0.1)
+
+        for k, effect in QuanticClusterHit do 
+            local effect = CreateEmitterAtBone(self, 'light_2', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        for k, effect in QuanticClusterMuzzleFlash do 
+            local effect = CreateEmitterAtBone(self, 'light_2', self.Army, effect)
+            effect:ScaleEmitter(0.35)
+        end
+
+        WaitSeconds(0.1)
+
+        self:HideBone('UAB5205_2', false)
+
+        WaitSeconds(0.1)
+
+        AStructureUnit.DeathThread(self, instigator, type, overkillRatio)
+    end,
+
+    -- override to disable default damage effects
+    CreateDestructionEffects = function() end,
 }
 
-TypeClass = UAB5103
+TypeClass = UAB5205
