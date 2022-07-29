624c774248cc7140bed864f19a7a7b2073546a29
Some Dev <somedeveloper05@gmail.com>
2022-07-29 17:23:39 +0300

    Improved player awareness
    Added a viewpoint to the player's death location.
    Added display of a system message about the death of a player.
    Added information about the army of the killed player to the beacon name.

--- mods/battle-royale/modules/sim/ACUInfo.lua
+++ mods/battle-royale/modules/sim/ACUInfo.lua
@@ -1,6 +1,7 @@
 --- An object containing the coordinates of the death of the ACU,
 --- necessary to create a commander's beacon.
 ACUDeathCoordinates = { }
+PlayersNicknames = { }
 
 --- Stores ACU death coordinates, armyIndex is needed to identify specific ACU.
 function ACUDeathCoordinates:SetCoords(armyIndex, x, y, z)
@@ -11,3 +12,12 @@ end
 function ACUDeathCoordinates:GetCoords(armyIndex)
     return self[armyIndex]
 end
+
+function PlayersNicknames:SetNickname(armyIndex, nickname)
+    self[armyIndex] = {nickname}
+end
+
+function PlayersNicknames:GetNickname(armyIndex)
+    return self[armyIndex]
+end
+
