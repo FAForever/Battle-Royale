0dd59f3e4adfc49575d043760d695f900f0923db
Some Dev <somedeveloper05@gmail.com>
2022-12-29 12:04:29 +0300

    Vault commit

--- mod_info.lua
+++ mod_info.lua
@@ -1,20 +1,20 @@
 
 -- name of the mod as seen in the mod manager and the client, used by the database to
 -- group mods.
-name = "Battle Royale"
+name = "Battle Royale by Dark Horse"
 
 -- version of the mod as seen in the mod manager and is used by the client to know
 -- what the latest version is
-version = 16
+version = 17
 
 -- used by the client to automatically download this mod, must be unique and updated
 -- when releasing a new version
-uid = "battle-royale-16"
+uid = "battle-royale-by-dark-horse-17"
 
 copyright = "GNU-3"
 description = "This mod requires the UI mod Dear Windowing. You need to enable it separarely. Without it you will have no UI. A battle royale mod including (free of charge) care packages! This mod is maintained by the community. You can find it on the FAForever group on Github."
-author = "(Jip) Willem Wijnia, Some Developer (Discord - DarkHorse#3513)"
-url = "https://github.com/FAForever/Battle-Royale"
+author = "Some Developer (Discord - DarkHorse#3513)"
+url = "https://github.com/SomeDev-v0-9/Battle-Royale-by-Dark-Horse"
 
 -- <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>               title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 icon = "/mods/battle-royale/icon.png"
